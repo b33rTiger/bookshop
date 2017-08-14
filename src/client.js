833499f35aa7265f462fca1e67752b96a17d3abe
John <john@johnsheets.me>
2017-08-14 16:28:44 -0700

    client connected for all API requests

--- src/client.js
+++ src/client.js
@@ -5,7 +5,8 @@ import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import {applyMiddleware,createStore} from 'redux';
-import logger from 'redux-logger';
+import {createLogger} from 'redux-logger';
+import thunk from 'redux-thunk';
 import reducers from './reducers/index';
 import {postBooks,deleteBooks,updateBooks} from './actions/booksActions';
 import BooksList from './components/pages/booksList';
@@ -13,7 +14,7 @@ import Cart from './components/pages/cart';
 import BooksForm from './components/pages/booksForm';
 import Main from './main';
 
-const middleware=applyMiddleware(logger);
+const middleware=applyMiddleware(thunk, createLogger());
 const store = createStore(reducers,middleware);
 const Routes = (
   <Provider store={store}>
