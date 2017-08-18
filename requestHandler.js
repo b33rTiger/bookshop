586dc530d0e086fb9dd4d819e3766d528c89d678
John <john@johnsheets.me>
2017-08-18 11:35:17 -0700

    working on initialState

--- requestHandler.js
+++ requestHandler.js
@@ -1,12 +1,21 @@
 "use strict"
 
 import axios from 'axios';
+import React from 'react';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {renderToString} from 'react-dom/server';
+import {match, RouterContext} from 'react-router';
+import reducers from './src/reducers/index';
+import routes from './src/routes';
 
 function handleRender(req, res){
   axios.get('http://localhost:3001/books')
     .then(function(response){
-      var myHtml = JSON.stringify(response.data);
-      res.render('index', {myHtml});
+      // var myHtml = JSON.stringify(response.data);
+      // res.render('index', {myHtml});
+      const store = createStore(reducers, {"books":{"books":response.data}});
+      const initialState = JSON.stringify(store.getState()).replace(/<\/script/g,'<\\/script').replace(/<!--/g, '<\\!--');
     })
     .catch(function(err){
       console.log('#Initial Server-side rendering error', err);
