7939262f8fb835c0d9446cea6f3ac81c2417db38
John <john@johnsheets.me>
2017-08-18 10:02:53 -0700

    ejs installed. request handler working client side

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+require('babel-core/register')({
+  "presets":["es2015", "react", "stage-1"]
+});
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var httpProxy = require('http-proxy');
+var requestHandler = require('./requestHandler.js');
 var app = express();
 
 const apiProxy = httpProxy.createProxyServer({
@@ -21,9 +26,8 @@ app.use('/api', function(req, res){
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', function(req, res){
- res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-})
+app.set('view engine', 'ejs');
+app.use(requestHandler);
 
 
 // catch 404 and forward to error handler
