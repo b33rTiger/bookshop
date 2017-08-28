fe0042957b8facd9d66f3241567372e932578a01
John <john@johnsheets.me>
2017-08-28 10:28:14 -0700

    app complete

--- requestHandler.js
+++ requestHandler.js
@@ -16,6 +16,26 @@ function handleRender(req, res){
       // res.render('index', {myHtml});
       const store = createStore(reducers, {"books":{"books":response.data}});
       const initialState = JSON.stringify(store.getState()).replace(/<\/script/g,'<\\/script').replace(/<!--/g, '<\\!--');
+      const Routes ={
+        routes:routes,
+        location:req.url
+      };
+      match(Routes, function(error, redirect, props){
+        if(error){
+          res.status(500).send("Error fullfilling the request");
+        } else if(redirect){
+          res.status(302, redirect.pathname + redirect.search)
+        } else if(props){
+          const reactComponent = renderToString(
+            <Provider store={store}>
+              <RouterContext {...props}/>
+            </Provider>
+          )
+          res.status(200).render('index', {reactComponent, initialState})
+        } else {
+          res.status(404).send('Not Found')
+        }
+      })
     })
     .catch(function(err){
       console.log('#Initial Server-side rendering error', err);
